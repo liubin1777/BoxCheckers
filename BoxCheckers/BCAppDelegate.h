463c839d075e25423227d6a6879c9a1e712afd69
Andrew Carter <andrew.carter@willowtreeapps.com>
2012-04-13 22:47:49 -0400

    Initial Commit

---
+++ BoxCheckers/BCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BCAppDelegate.h
+//  BoxCheckers
+//
+//  Created by Andrew Carter on 4/13/12.
+//  Copyright (c) 2012 WillowTree Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BoxCheckers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BoxCheckers
+//
+//  Created by Andrew Carter on 4/13/12.
+//  Copyright (c) 2012 WillowTree Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BCAppDelegate class]));
+    }
+}
